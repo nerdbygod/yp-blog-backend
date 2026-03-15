b9d4c5aed8d22ba1a4aae6769f9561ad7408ae8e
Pavel Artemov <pavelartemov53@gmail.com>
2026-03-15 23:09:43 +0500

    SP-3: Add DataSourceConfiguration and application.properties file for database configuration

---
+++ src/main/java/org/practicum/yandex/config/DataSourceConfiguration.java
@@ -0,0 +1,55 @@
+package org.practicum.yandex.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+import org.postgresql.Driver;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    public DataSource dataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password
+    ) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Driver.class.getName());
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void prepareDatabase(ContextRefreshedEvent event) {
+        final var dataSource = event.getApplicationContext().getBean(DataSource.class);
+
+        final var populator = new ResourceDatabasePopulator();
+
+        populator.addScript(new ClassPathResource("sql/schema.sql"));
+        populator.execute(dataSource);
+    }
+
+}
