b249d5998661741fe4041605e7da2479efbfbe25
Pavel Artemov <pavelartemov53@gmail.com>
2026-03-16 23:43:10 +0500

    SP-3: Fix handling generic exception in ResponseExceptionHandler
    - extend ResponseEntityExceptionHandler and use its handleExceptionInternal() method for proper error representation
    - add ErrorDto to map details

--- src/main/java/org/practicum/yandex/controller/exception/ResponseExceptionHandler.java
+++ src/main/java/org/practicum/yandex/controller/exception/ResponseExceptionHandler.java
@@ -1,16 +1,20 @@
 package org.practicum.yandex.controller.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.UUID;
 
 @Slf4j
 @ControllerAdvice
-public class ResponseExceptionHandler {
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Object> handleResponseStatus(ResponseStatusException rse) {
@@ -18,11 +22,21 @@ public class ResponseExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleRuntimeException(Exception exception,
+    public ResponseEntity<Object> handleGenericException(Exception exception,
                                                          WebRequest request) {
-        log.warn("{} Unhandled error occurred while processing request: ", request.getDescription(false), exception);
+        final var uuid = UUID.randomUUID().toString();
 
-        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unhandled error");
+        log.warn("[{}] [{}] Unhandled error occurred while processing request",
+                request.getDescription(false), uuid, exception
+        );
+
+        final var body = ErrorDto.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+                .message("Unhandled exception occurred")
+                .id(uuid)
+                .build();
+
+        return handleExceptionInternal(exception, body, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 
 }
