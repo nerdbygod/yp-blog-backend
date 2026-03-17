c1ce58a50f60973f0ac9ebf5ef85cd771d2d766a
Pavel Artemov <pavelartemov53@gmail.com>
2026-03-18 01:35:18 +0500

    SP-3: Relax validation of CreatePostRequest in RequestValidator
    - make tags optional

--- src/main/java/org/practicum/yandex/service/validation/RequestValidator.java
+++ src/main/java/org/practicum/yandex/service/validation/RequestValidator.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.practicum.yandex.controller.dto.CommentDto;
 import org.practicum.yandex.controller.dto.request.AddCommentRequest;
 import org.practicum.yandex.controller.dto.request.CreatePostRequest;
-import org.springframework.util.CollectionUtils;
 
 import java.util.Objects;
 
@@ -14,9 +13,7 @@ import java.util.Objects;
 public class RequestValidator {
     public static boolean isInvalid(final CreatePostRequest request) {
         return StringUtils.isBlank(request.getText()) ||
-                StringUtils.isBlank(request.getTitle()) ||
-                CollectionUtils.isEmpty(request.getTags()) ||
-                request.getTags().stream().noneMatch(StringUtils::isNotBlank);
+                StringUtils.isBlank(request.getTitle());
     }
 
     public static boolean isInvalid(final AddCommentRequest request) {
