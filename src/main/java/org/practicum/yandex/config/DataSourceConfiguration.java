e565a2285da534f5372c425faaeaf5ecacb8561c
Pavel Artemov <pavelartemov53@gmail.com>
2026-03-23 21:10:49 +0500

    SP-3: Use HikariCP instead of DriverManagerDataSource to manage db connections

--- src/main/java/org/practicum/yandex/config/DataSourceConfiguration.java
+++ src/main/java/org/practicum/yandex/config/DataSourceConfiguration.java
@@ -1,5 +1,8 @@
 package org.practicum.yandex.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.postgresql.Driver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,28 +11,32 @@ import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
-import org.postgresql.Driver;
 
 @Configuration
 public class DataSourceConfiguration {
 
     @Bean
-    public DataSource dataSource(
+    public HikariConfig hikariConfig(
             @Value("${spring.datasource.url}") String url,
             @Value("${spring.datasource.username}") String username,
             @Value("${spring.datasource.password}") String password
     ) {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(Driver.class.getName());
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        final var config = new HikariConfig();
 
-        return dataSource;
+        config.setDriverClassName(Driver.class.getName());
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        return config;
+    }
+
+    @Bean
+    public DataSource hikariDataSource(HikariConfig hikariConfig) {
+        return new HikariDataSource(hikariConfig);
     }
 
     @Bean
